f3c44ac01ec919868f7a4f19ef7bf9ecc72a3558
linhdv <dovanlinh257200@gmail.com>
2024-07-14 16:06:21 +0700

    testing multiple thread task consumer

--- account-service/src/main/java/com/enthusiasm/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/enthusiasm/accountservice/AccountServiceApplication.java
@@ -1,13 +1,16 @@
 package com.enthusiasm.accountservice;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AccountServiceApplication.class, args);
+        new SpringApplicationBuilder(AccountServiceApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
     }
-
 }
--- payment-service/src/main/java/com/enthusiasm/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/enthusiasm/paymentservice/PaymentServiceApplication.java
@@ -1,5 +1,6 @@
 package com.enthusiasm.paymentservice;
 
+import com.enthusiasm.paymentservice.kafka.MultiThreadedKafkaConsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,6 @@ public class PaymentServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
+        MultiThreadedKafkaConsumer consumer = new MultiThreadedKafkaConsumer("test-topic");
     }
-
 }
