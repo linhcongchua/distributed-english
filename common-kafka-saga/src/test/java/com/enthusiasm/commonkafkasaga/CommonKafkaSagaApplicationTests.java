6d6bfadfbe1681e1868b71be3e224a2cd7a35b21
linhdv <dovanlinh257200@gmail.com>
2024-07-14 16:07:19 +0700

    add common-kafka-saga

---
+++ common-kafka-saga/src/test/java/com/enthusiasm/commonkafkasaga/CommonKafkaSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enthusiasm.commonkafkasaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonKafkaSagaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
