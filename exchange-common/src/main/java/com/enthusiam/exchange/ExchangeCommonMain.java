912949868bea432b1a1bebcfbd5a3a8a3441f04b
linhdv <dovanlinh257200@gmail.com>
2025-03-31 23:12:02 +0700

    upgrade spring

---
+++ exchange-common/src/main/java/com/enthusiam/exchange/ExchangeCommonMain.java
@@ -0,0 +1,11 @@
+package com.enthusiam.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeCommonMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeCommonMain.class);
+    }
+}
